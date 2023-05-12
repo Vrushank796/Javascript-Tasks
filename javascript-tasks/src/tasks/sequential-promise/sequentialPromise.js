3e673a1e84381738cb50ff12fdddc3b8e2d077bc
Vrushank Amin <42446203+Vrushank796@users.noreply.github.com>
2023-05-12 00:46:59 +0000

    Update sequentialPromise.js and sumFibsMemoized.js

--- javascript-tasks/src/tasks/sequential-promise/sequentialPromise.js
+++ javascript-tasks/src/tasks/sequential-promise/sequentialPromise.js
@@ -1,21 +1,28 @@
 function sequentialPromise(promises) {
-  
+  return new Promise((resolve, reject) => {
+    Promise.allSettled(promises).then((results) => {
+      results.forEach((result) => {
+        console.log(result);
+      });
+    });
+  });
 }
 
 // export { sequentialPromise };
 
 const promise1 = new Promise((resolve, reject) => {
-  setTimeout(resolve, 2000, 'foo');
+  setTimeout(resolve, 2000, "foo");
 });
 const promise2 = new Promise((resolve, reject) => {
-  setTimeout(resolve, 2000, 'foo');
+  setTimeout(resolve, 2000, "foo");
 });
 const promise3 = new Promise((resolve, reject) => {
-  setTimeout(resolve, 2000, 'foo');
+  setTimeout(resolve, 2000, "foo");
 });
 
-sequentialPromise([promise1, promise2, promise3]).then((value) => console.log(value));
-
+sequentialPromise([promise1, promise2, promise3])
+  .then((value) => console.log(value))
+  .catch(console.log("Promise Rejected"));
 // const timeBeforeStarting = performance.now();
 // await sequentialPromise(promiseFunctions);
 // const timeAfterEnding = performance.now();
--- javascript-tasks/src/tasks/sum-fibs-memoized/sumFibsMemoized.js
+++ javascript-tasks/src/tasks/sum-fibs-memoized/sumFibsMemoized.js
@@ -1,9 +1,57 @@
 function sumFibs(num) {
-  
+  if (typeof num === "number") {
+    let temp = [1, 1];
+    let sum = 0;
+
+    //create array with fibonnaci numbers less than num
+    for (let i = 2; i < num; i++) {
+      let tempSum = temp[i - 1] + temp[i - 2];
+      if (tempSum < num) {
+        temp.push(tempSum);
+      } else {
+        break;
+      }
+    }
+
+    //calculate fibonnaci sum of odd fibonnaci numbers less than 10
+    temp.map((x) => {
+      if (x % 2 != 0) {
+        sum = sum + x;
+      }
+    });
+    return sum;
+  } else {
+    return num;
+  }
 }
 
+//memoized function-> save previous result using closure and lexical scoping
 function cacheFunction(fn) {
-  
+  var sum = 0;
+  var boolean = false;
+  return function (fn) {
+    if (typeof fn !== "number") {
+      //for boolean argument
+      if (boolean == false) {
+        boolean = sumFibs(fn);
+        console.log("first" + boolean);
+        return sum;
+      } else {
+        console.log("second" + boolean);
+        return sum;
+      }
+    } else {
+      //for number argument
+      if (sum == 0) {
+        sum = sumFibs(fn);
+        console.log("first" + sum);
+        return sum;
+      } else {
+        console.log("second" + sum);
+        return sum;
+      }
+    }
+  };
 }
 
 export { sumFibs, cacheFunction };
