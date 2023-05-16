41c68080ea93a1f9992282281242b64a010bfb32
Vrushank Amin <42446203+Vrushank796@users.noreply.github.com>
2023-05-16 19:52:46 +0000

    Update balancedBraces.js

--- javascript-tasks/src/tasks/balanced-braces/balancedBraces.js
+++ javascript-tasks/src/tasks/balanced-braces/balancedBraces.js
@@ -1,11 +1,9 @@
 function balancedBraces(str) {
   const para = { "}": "{", "]": "[", ")": "(" };
-  // let countOpenBr = 0;
-  // let countCloseBr = 0;
+
   let tags = [];
   for (let i in str) {
     if (str[i] == "{" || str[i] == "[" || str[i] == "(") {
-      // countOpenBr += 1;
       tags.push(str[i]);
     } else if (str[i] == "}" || str[i] == "]" || str[i] == ")") {
       if (para[str[i]] == tags[tags.length - 1]) {
