7b8ae5bb85dcd58275263bb50f33859a734f5d27
Vrushank Amin <42446203+Vrushank796@users.noreply.github.com>
2023-05-16 19:57:44 +0000

    Update balancedBraces.js

--- javascript-tasks/src/tasks/balanced-braces/balancedBraces.js
+++ javascript-tasks/src/tasks/balanced-braces/balancedBraces.js
@@ -1,16 +1,22 @@
 function balancedBraces(str) {
+  //para object -> To compare with opening tag in tags array when closing tag encounters
   const para = { "}": "{", "]": "[", ")": "(" };
 
-  let tags = [];
+  let tags = []; // To store opening tag
+
+  //loop through characters of string
   for (let i in str) {
     if (str[i] == "{" || str[i] == "[" || str[i] == "(") {
-      tags.push(str[i]);
+      tags.push(str[i]); // Push opening tag in array
     } else if (str[i] == "}" || str[i] == "]" || str[i] == ")") {
       if (para[str[i]] == tags[tags.length - 1]) {
+        //Compare last opening tag in tags array, if it is the same one remove it.
         tags.pop();
       }
     }
   }
+
+  //Check if tags array is empty, we would have balanced braces, paranthesis, and brackets
   if (tags.length == 0) {
     return true;
   } else {
