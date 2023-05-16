13d3e8def02a2fa5b4d2c48e7555937a76c438a6
Vrushank Amin <42446203+Vrushank796@users.noreply.github.com>
2023-05-16 18:44:01 +0000

    Update chunkArrayInGroups.js

--- javascript-tasks/src/tasks/chunk-array-in-groups/chunkArrayInGroups.js
+++ javascript-tasks/src/tasks/chunk-array-in-groups/chunkArrayInGroups.js
@@ -1,8 +1,9 @@
 /* eslint-disable no-param-reassign */
 function chunkArrayInGroups(array, chunkSize) {
-  
+  const chunk = [];
+  for (let i = 0; i < array.length; i += chunkSize)
+    chunk.push(array.slice(i, i + chunkSize));
+  return chunk;
 }
 
-export {
-  chunkArrayInGroups,
-};
+export { chunkArrayInGroups };
